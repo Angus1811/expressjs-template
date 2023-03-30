a8fc48ef6a930a16fd4b7ae419fe3843b3cbac6a
Angus1811 <102298861+Angus1811@users.noreply.github.com>
2023-03-30 17:45:48 +0100

    express

---
+++ main.js
@@ -0,0 +1,33 @@
+const express = require('express');
+var cors = require('cors')
+const app = express()
+const PORT = 80;
+app.use(express.json());
+app.use(
+    cors({
+        origin: '*',
+    })
+)
+
+app.get('/example', (req, res) => {
+    res.status(200).send({
+        "JSON": "javascript object notation",
+    });
+})
+
+app.post('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    res.status(200).send({
+        "id": id,
+        "name": name,
+        "email": email
+    });
+  });
+
+app.listen(
+    PORT,
+    () => console.log(`listening on http://localhost:${80}`)
+    //port 80 maybe in use (try another port)
+)
